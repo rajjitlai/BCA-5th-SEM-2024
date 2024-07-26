d1f59a4a3ec4e30617af9df387bf52fff84ebaac
RAJJIT <2.0lairaj@gmail.com>
2024-07-26 10:34:58 +0530

    date-26-1

---
+++ NECB/date-26/star.js
@@ -0,0 +1,9 @@
+let item = "";
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        item += "* ";
+    }
+    item += "\n";
+}
+console.log(item);
+
---
+++ NECB/date-26/variable.js
@@ -0,0 +1,6 @@
+const a = "";
+
+// a = '1' assignment to constant variable is not allowed
+
+console.log(a)
+
