e046740b646d108783fd3186fa7257d1346713a2
RAJJIT <2.0lairaj@gmail.com>
2024-07-28 09:45:13 +0530

    date-26-map-function

---
+++ NECB/date-26/mapFunction.js
@@ -0,0 +1,22 @@
+const Data = [
+    {
+        id: 1,
+        name: 'John',
+        age: 25
+    },
+    {
+        id: 2,
+        name: 'Jane',
+        age: 30
+    },
+]
+
+const result = Data.map((item, idx) => {
+    console.log({ "item...": item, "index...": idx });
+});
+
+// Output:
+/*
+{ 'item...': { id: 1, name: 'John', age: 25 }, 'index...': 0 }
+{ 'item...': { id: 2, name: 'Jane', age: 30 }, 'index...': 1 }
+*/
