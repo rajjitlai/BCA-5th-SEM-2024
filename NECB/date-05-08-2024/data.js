1ce425f22c345c3fd030bde7618e373ac66a68f2
RAJJIT <2.0lairaj@gmail.com>
2024-08-05 11:21:06 +0530

    05-08-json-object-extra

--- NECB/date-05-08-2024/data.js
+++ NECB/date-05-08-2024/data.js
@@ -1,12 +1,15 @@
-const data = {
+const data = [
+    {
     name: 'John Doe',
     age: 30,
-    address: {
-        street: '123 Main St',
-        city: 'New York',
-        state: 'NY'
+    address: "California, USA"
+    },
+    {
+        name: 'Jane Doe',
+        age: 28,
+        address: "New York, USA"
     }
-}
+]
 
 let data2Json = JSON.stringify(data);
 const jsonData = require('./data.json')
@@ -14,12 +17,22 @@ const jsonData = require('./data.json')
 let json2Data = JSON.stringify(jsonData)
 
 // without stringify
-console.log({"data...": jsonData})
+// console.log({"data...": jsonData})
 
 const data2Object = JSON.parse(data2Json)
 
-console.log(data2Json)
-console.log(jsonData)
-console.log(json2Data)
+// console.log(data2Json)
+// console.log(jsonData)
+// console.log(json2Data)
 
-console.log(data2Object)
+// console.log(data2Object)
+
+// console.log(data)
+
+// mapping objects
+data.map((key) => {
+    // console.log(key)
+    if(key.name == "John Doe"){
+        console.log(key)
+    }
+})
