2628c6f3fd175dc4a1cdd8e15a883951b543920e
RAJJIT <2.0lairaj@gmail.com>
2024-08-07 11:19:52 +0530

    07-08-added-updated-put

--- NECB/date-06-08-2024/app/[id]/route.ts
+++ NECB/date-06-08-2024/app/[id]/route.ts
@@ -4,15 +4,17 @@ async function UpdateUser(id: string | null, data: any) {
     return {success: true, message: `User ${id} updated`, data };
 }
 
-export async function PUT(req: NextRequest){
+export async function PUT(req: NextRequest,{params}:{params:any}){
     try{
-        const {searchParams} = new URL(req.url)
-        const id = searchParams.get('id')
+        // const {searchParams} = new URL(req.url)
+        // const id = searchParams.get('id')
+        
+        const id = params.id
+        // console.log(id)
 
-        const json = await req.json()
-        const newData = json.data;
-
-        const result = await UpdateUser(id, newData)
+        const name = await req.json()
+        
+        const result = await UpdateUser(id, name)
 
         return NextResponse.json(result, {status: 200})
     }catch(error){
