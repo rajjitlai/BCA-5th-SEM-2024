75f06b40994c66d569430d9ab1043138b7c40c55
RAJJIT <2.0lairaj@gmail.com>
2024-08-08 13:46:53 +0530

    07-08-changes

--- NECB/date-06-08-2024/app/[id]/route.ts
+++ NECB/date-06-08-2024/app/[id]/route.ts
@@ -1,20 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
-async function UpdateUser(id: string | null, data: any) {
-    return {success: true, message: `User ${id} updated`, data };
+async function UpdateUser(id: string | null) {
+    return {success: true, message: `User ${id} updated`};
 }
 
 export async function PUT(req: NextRequest,{params}:{params:any}){
     try{
         // const {searchParams} = new URL(req.url)
         // const id = searchParams.get('id')
+        // const name = await req.json()
         
         const id = params.id
         // console.log(id)
 
-        const name = await req.json()
         
-        const result = await UpdateUser(id, name)
+        const result = await UpdateUser(id)
 
         return NextResponse.json(result, {status: 200})
     }catch(error){
