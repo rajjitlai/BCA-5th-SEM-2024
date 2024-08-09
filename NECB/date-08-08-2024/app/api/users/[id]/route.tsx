aa8ae762cac8def87e9d2ac10ae108ccd18cb9b2
RAJJIT <2.0lairaj@gmail.com>
2024-08-09 10:38:06 +0530

    09-08-NECB-get-same-category-user&product

--- NECB/date-08-08-2024/app/api/users/[id]/route.tsx
+++ NECB/date-08-08-2024/app/api/users/[id]/route.tsx
@@ -1,22 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const data = [
-    {
-        id: '1',
-        name: 'jack',
-        department: 'cse'
-    },
-    {
-        id: '2',
-        name: 'jill',
-        department: 'eee'
-    }
-]
+const users = require('../users.json')
+const products = require('../../products/products.json')
+
+// const data = [
+//     {
+//         id: '1',
+//         name: 'jack',
+//         department: 'cse'
+//     },
+//     {
+//         id: '2',
+//         name: 'jill',
+//         department: 'eee'
+//     }
+// ]
+
+// export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+//     if (params.id == data[0].id) {
+//         return NextResponse.json(data[0])
+//     } else {
+//         return NextResponse.json({ status: 404, message: 'Id not found' })
+//     }
+// }
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    if (params.id == data[0].id) {
-        return NextResponse.json(data[0])
-    } else {
-        return NextResponse.json({ status: 404, message: 'Id not found' })
+    const userId = params.id;
+    const user = users.find((u: any) => u.id === userId)
+    console.log({"user": user})
+
+    if (!user) {
+        return NextResponse.json({ message: 'User not found' }, { status: 404 })
     }
+
+    const userCategory = user.category;
+    console.log({ "userCategory....": userCategory })
+
+    const check = products.filter((products: any) => products.category === userCategory)
+    console.log({ "check.....": check })
+
+    return NextResponse.json({ user, products: check }, { status: 200 })
 }
--- NECB/date-08-08-2024/app/api/users/route.tsx
+++ NECB/date-08-08-2024/app/api/users/route.tsx
@@ -1,14 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 
-const data = require('../users/users.json')
-
-export function GET() {
-    return NextResponse.json(data, { status: 200 });
-}
-
-export async function POST(req: NextRequest) {
-    const body = await req.json();
-    data.push(body);
-    console.log(body);
-    return NextResponse.json(body, { status: 200 });
-}
+// get the users data
+// export function GET() {
+//     return NextResponse.json(data, { status: 200 });
+// }
+// post a user 
+// export async function POST(req: NextRequest) {
+//     const body = await req.json();
+//     data.push(body);
+//     console.log(body);
+//     return NextResponse.json(body, { status: 200 });
+// }
