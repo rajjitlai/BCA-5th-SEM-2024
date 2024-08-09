c0dd363e6008af5f07f52762d2313db8e140317e
RAJJIT <2.0lairaj@gmail.com>
2024-08-09 10:52:42 +0530

    09-08-NECB-changes

--- NECB/date-08-08-2024/app/api/users/route.tsx
+++ NECB/date-08-08-2024/app/api/users/route.tsx
@@ -1,13 +1,16 @@
 import { NextRequest, NextResponse } from "next/server"
 
+const data = require('./users.json')
+
 // get the users data
-// export function GET() {
-//     return NextResponse.json(data, { status: 200 });
-// }
+export function GET() {
+    return NextResponse.json(data, { status: 200 });
+}
+
 // post a user 
-// export async function POST(req: NextRequest) {
-//     const body = await req.json();
-//     data.push(body);
-//     console.log(body);
-//     return NextResponse.json(body, { status: 200 });
-// }
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    data.push(body);
+    console.log(body);
+    return NextResponse.json(body, { status: 200 });
+}
