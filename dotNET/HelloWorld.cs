6537389f0a78b4cffb2ec2eecde8a50a2f05adb4
RAJJIT <2.0lairaj@gmail.com>
2024-08-12 19:49:56 +0530

    12-08-added-csharp-1st-code

---
+++ dotNET/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            console.Write("Hello World");
+            console.ReadKey();
+        }
+    }
+}
