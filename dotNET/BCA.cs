097517bf1a409d1bae53955ced89489f3a11c48e
RAJJIT <2.0lairaj@gmail.com>
2024-08-20 22:29:05 +0530

    19-08-2024-dotnet-namespace

---
+++ dotNET/BCA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Program to illustrate use of multiple namespace
+namespace BCA
+{
+    class A
+    {
+        public void Msg() => Console.Write("Welcome to BCA");
+    }
+
+    class B
+    {
+        public static void Main(String[] args)
+        {
+            A obj = new A();
+            obj.Msg();
+            Console.Read();
+        }
+    }
+}
