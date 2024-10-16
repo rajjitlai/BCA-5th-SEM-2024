84fb3967f7b6d0a10f827edb36e5e5587ab91dda
RAJJIT <2.0lairaj@gmail.com>
2024-10-16 20:29:49 +0530

    dotnet-added-delegate-example

---
+++ dotNET/theory/delegateExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace delegateEg
+{
+    // declaring delegate
+    public delegate void dd(int x, int y);
+
+    class A
+    {
+        public void addition(int a, int b)
+        {
+            Console.WriteLine("Addition = {0}", a+b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            dd obj2 = obj.addition;
+            obj2(5, 10);
+        }
+    }
+}
