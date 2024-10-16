beef28bf295907410275c75176d89525ac94a72a
RAJJIT <2.0lairaj@gmail.com>
2024-10-16 20:53:07 +0530

    dotnet-added-Stack-example

---
+++ dotNET/theory/StackNonGeneric/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace stackExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();  
+            stack.Push("Hello");        
+            stack.Push(120);
+            stack.Push(13.23);
+
+            Console.WriteLine("Stack Elements are:");
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"\nPopped Element: {stack.Pop()}");
+
+            Console.WriteLine($"\nTop Element after Pop: {stack.Peek()}");
+        }
+    }
+}
