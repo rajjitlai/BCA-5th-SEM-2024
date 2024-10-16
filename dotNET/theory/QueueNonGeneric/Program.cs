e02c9b15a176b8c4bf96892f542d6321eca09a00
RAJJIT <2.0lairaj@gmail.com>
2024-10-16 20:57:43 +0530

    dotnet-added-queue-example

---
+++ dotNET/theory/QueueNonGeneric/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace queueExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("Hello");  
+            queue.Enqueue(120);
+            queue.Enqueue(13.23);
+
+            Console.WriteLine("Queue Elements are:");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"\nDequeued Element: {queue.Dequeue()}");
+
+            Console.WriteLine($"\nFront Element after Dequeue: {queue.Peek()}");
+        }
+    }
+}
