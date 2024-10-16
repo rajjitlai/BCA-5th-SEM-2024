7a68bb23799d54eb44a2b3085b368d0a04a14313
RAJJIT <2.0lairaj@gmail.com>
2024-10-16 20:39:15 +0530

    dotnet-added-arrayList-example

---
+++ dotNET/theory/ArrayListNonGeneric/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+namespace arrayList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList arrList = new ArrayList();
+        arrList.Add("Hello");
+        arrList.Add(120);
+        arrList.Add(13.23);
+        Console.Write("Array List Elements are: ");
+        foreach(var item in arrList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
