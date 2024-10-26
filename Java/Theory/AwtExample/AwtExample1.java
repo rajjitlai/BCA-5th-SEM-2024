abb08c2bd6a926d0d57b328c61801134a49be1ce
Rajjit Laishram <2.0lairaj@gmail.com>
2024-10-26 09:20:58 +0530

    java-awt-fixed

--- Java/Theory/AwtExample/AwtExample1.java
+++ Java/Theory/AwtExample/AwtExample1.java
@@ -3,13 +3,14 @@ package AwtExample;
 import java.awt.*;
 
 public class AwtExample1 extends Frame {
-    void AWTExample1() {
+    public AwtExample1() {
         Button b = new Button("Click Me!!");
         b.setBounds(30, 100, 80, 30);
         add(b);
         setTitle("AWT Example");
         setLayout(null);
 
+        setSize(300, 200);
         setVisible(true);
     }
 
