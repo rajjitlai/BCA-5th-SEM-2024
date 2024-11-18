7faa0bff74ac0ab46228c13469bee1b98f7f8420
RAJJIT <2.0lairaj@gmail.com>
2024-11-18 21:10:12 +0530

    net-practice-sort-array

---
+++ dotNET/theory/SortArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortArray
+{
+    class Sorting
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 2, 9, 1, 7, 3 };
+            Array.Sort(numbers);
+            Console.WriteLine("Sorted array: ");
+            foreach (int number in numbers) { 
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
