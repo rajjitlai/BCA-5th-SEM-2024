78de34c80b35977be99d38c4d2992cd8f51bea8e
RAJJIT <2.0lairaj@gmail.com>
2024-11-18 21:16:05 +0530

    net-practice-reversed-array

---
+++ dotNET/theory/ReverseArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ReverseArray
+{
+    class Reversing
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 2, 9, 1, 7, 3 };
+            Array.Reverse(numbers);
+            Console.WriteLine("Reversed Array: ");
+            foreach(int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
